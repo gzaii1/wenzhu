fecc8b1cfc7c19d7915036bb625ba9b1f93867e9
guohanbj <guohanbj@yuanfudao.com>
2022-11-13 00:11:32 +0800

    test: 测试错误提交

--- packages/home/webpack.config.js
+++ packages/home/webpack.config.js
@@ -1,27 +1,27 @@
 import path, { dirname } from 'path'
 import HtmlWebpackPlugin from 'html-webpack-plugin'
 import { fileURLToPath } from 'url'
-
+console.log(__dirname)
 const _filename = fileURLToPath(import.meta.url)
 const _dirname = dirname(_filename)
 export default function () {
-    return {
-        entry: `${_dirname}/src/index.js`,
-        mode: 'development',
-        output: {
-            filename: '[name].js',
-            path: path.resolve(_dirname, 'dist')
-        },
-        devServer: {
-            hot: true,
-            open: true,
-            port: 8081,
-        },
-        cache: false,
-        plugins: [
-            new HtmlWebpackPlugin({
-                template: `${_dirname}/public/template.html`
-            })
-        ]
-    }
+  return {
+    entry: `${_dirname}/src/index.js`,
+    mode: 'development',
+    output: {
+      filename: '[name].js',
+      path: path.resolve(_dirname, 'dist')
+    },
+    devServer: {
+      hot: true,
+      open: true,
+      port: 8081
+    },
+    cache: false,
+    plugins: [
+      new HtmlWebpackPlugin({
+        template: `${_dirname}/public/template.html`
+      })
+    ]
+  }
 }
